81cf1379d48a42de3b281d878f84b7874b73416d
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-10 13:45:37 -0400

    Auto-formatting code

--- main.py
+++ main.py
@@ -10,49 +10,53 @@ from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.cluster import KMeans, AgglomerativeClustering
 
+
 def load_data(filepath):
     """
     Load customer data from a CSV file.
-    
+
     Parameters:
     filepath (str): Path to the CSV file
-    
+
     Returns:
     data (pd.DataFrame): Loaded customer data
     """
     return pd.read_csv(filepath)
 
+
 def handle_missing_values(data):
     """
     Handle missing values in the data by dropping rows with any missing values.
-    
+
     Parameters:
     data (pd.DataFrame): Customer data
-    
+
     Returns:
     data (pd.DataFrame): Customer data after handling missing values
     """
     return data.dropna()
 
+
 def encode_categorical_variables(data):
     """
     Encode categorical variables in the data using one-hot encoding.
-    
+
     Parameters:
     data (pd.DataFrame): Customer data
-    
+
     Returns:
     data (pd.DataFrame): Customer data after encoding categorical variables
     """
     return pd.get_dummies(data)
 
+
 def perform_feature_scaling(data):
     """
     Perform feature scaling on the data using StandardScaler.
-    
+
     Parameters:
     data (pd.DataFrame): Customer data
-    
+
     Returns:
     scaled_data (np.array): Scaled customer data
     scaler (StandardScaler): StandardScaler object used for scaling
@@ -61,33 +65,35 @@ def perform_feature_scaling(data):
     scaled_data = scaler.fit_transform(data)
     return scaled_data, scaler
 
+
 def exploratory_data_analysis(data):
     """
     Perform exploratory data analysis on the customer data.
-    
+
     Parameters:
     data (pd.DataFrame): Customer data
     """
     # Distribution of customer data
     data.describe()
-    
+
     # Correlation analysis
     corr_matrix = data.corr()
     sns.heatmap(corr_matrix, annot=True)
     plt.show()
-    
+
     # Visualize patterns
     sns.pairplot(data)
     plt.show()
 
+
 def feature_selection(data, target):
     """
     Perform feature selection on the data using Recursive Feature Elimination (RFE).
-    
+
     Parameters:
     data (pd.DataFrame): Customer data
     target (pd.Series): Target variable 
-    
+
     Returns:
     selected_features (pd.DataFrame): Selected features after feature selection
     """
@@ -96,14 +102,15 @@ def feature_selection(data, target):
     selected_features = rfe.fit_transform(features, target)
     return selected_features
 
+
 def train_models(X_train, y_train):
     """
     Train logistic regression, random forest, and gradient boosting models.
-    
+
     Parameters:
     X_train (np.array): Training data
     y_train (pd.Series): Training labels
-    
+
     Returns:
     logreg_model (LogisticRegression): Trained logistic regression model
     rf_model (RandomForestClassifier): Trained random forest model
@@ -112,22 +119,23 @@ def train_models(X_train, y_train):
     logreg_model = LogisticRegression()
     rf_model = RandomForestClassifier()
     gb_model = GradientBoostingClassifier()
-    
+
     logreg_model.fit(X_train, y_train)
     rf_model.fit(X_train, y_train)
     gb_model.fit(X_train, y_train)
-    
+
     return logreg_model, rf_model, gb_model
+
 
 def evaluate_model(model, X_test, y_test):
     """
     Evaluate the performance of a model by calculating accuracy, precision, recall, and F1-score.
-    
+
     Parameters:
     model: Trained model
     X_test (np.array): Test data
     y_test (pd.Series): Test labels
-    
+
     Returns:
     accuracy (float): Accuracy of the model
     precision (float): Precision of the model
@@ -139,44 +147,47 @@ def evaluate_model(model, X_test, y_test):
     precision = precision_score(y_test, pred)
     recall = recall_score(y_test, pred)
     f1 = f1_score(y_test, pred)
-    
+
     return accuracy, precision, recall, f1
+
 
 def cluster_data(data, n_clusters):
     """
     Perform customer segmentation by clustering the data using K-means and hierarchical clustering.
-    
+
     Parameters:
     data (pd.DataFrame): Customer data
-    
+
     n_clusters (int): Number of clusters
-    
+
     Returns:
     kmeans_labels (np.array): Cluster labels from K-means clustering
     hierarchical_labels (np.array): Cluster labels from hierarchical clustering
     """
     kmeans = KMeans(n_clusters=n_clusters)
     hierarchical = AgglomerativeClustering(n_clusters=n_clusters)
-    
+
     kmeans_labels = kmeans.fit_predict(data)
     hierarchical_labels = hierarchical.fit_predict(data)
-    
+
     return kmeans_labels, hierarchical_labels
+
 
 def visualize_heatmap(corr_matrix):
     """
     Visualize a heatmap of the correlation matrix.
-    
+
     Parameters:
     corr_matrix (pd.DataFrame): Correlation matrix
     """
     sns.heatmap(corr_matrix, annot=True)
     plt.show()
 
+
 def visualize_bar_chart(data, target):
     """
     Visualize a bar chart of the target variable.
-    
+
     Parameters:
     data (pd.DataFrame): Customer data
     target (str): Name of the target variable
@@ -187,10 +198,11 @@ def visualize_bar_chart(data, target):
     plt.ylabel('Count')
     plt.show()
 
+
 def visualize_line_graph(models, scores):
     """
     Visualize a line graph of model scores.
-    
+
     Parameters:
     models (list): Names of the models
     scores (list): Scores of the models
@@ -200,13 +212,14 @@ def visualize_line_graph(models, scores):
     plt.ylabel('Score')
     plt.show()
 
+
 def generate_report(score):
     """
     Generate a report with model evaluation metrics.
-    
+
     Parameters:
     score (float): Score of the logistic regression model
-    
+
     Returns:
     report (str): Generated report
     """
@@ -230,6 +243,7 @@ def generate_report(score):
 
     return report
 
+
 # Load the customer data
 data = load_data('customer_data.csv')
 
@@ -245,7 +259,8 @@ exploratory_data_analysis(data)
 selected_features = feature_selection(data, 'Churn')
 
 # Machine Learning Modeling
-X_train, X_test, y_train, y_test = train_test_split(selected_features, data['Churn'], test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    selected_features, data['Churn'], test_size=0.2, random_state=42)
 logreg_model, rf_model, gb_model = train_models(X_train, y_train)
 
 # Model Evaluation and Hyperparameter Tuning
